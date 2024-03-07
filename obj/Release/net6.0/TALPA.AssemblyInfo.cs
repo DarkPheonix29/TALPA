fc786bb1b83191ff38aab6ee58d003e2c76f4b1b
Kealan Thal <kealanthal29@gmail.com>
2024-03-07 11:41:03 +0100

    library test

--- obj/Release/net6.0/TALPA.AssemblyInfo.cs
+++ obj/Release/net6.0/TALPA.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TALPA")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73005224fbf176c4e3c5880bdbeffaacf28cc1f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TALPA")]
