d99c2d9009e956b7b6db7b58251108e8064ae0ed
DarkPheonix29 <kealanthal29@gmail.com>
2024-03-19 12:54:23 +0100

    avtivity limitations start

---
+++ DAL/Limits.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Limits
+    {
+        private string _limit;
+
+        public string Limit { get { return _limit; } set { this._limit = value; }}
+    }
+}
