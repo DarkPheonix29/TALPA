d9609adcc32942607ec1c8f4caada11da7bba8f9
Donners,Merijn M.B.J. <m.donners@student.fontys.nl>
2024-03-28 11:57:19 +0100

    changed UserName back to correct source

--- TALPA/Controllers/AccountController.cs
+++ TALPA/Controllers/AccountController.cs
@@ -61,7 +61,7 @@ namespace TALPA.Controllers
 		{
             var UserProfile = new UserProfile
             {
-				UserName = User.Identity.Name,
+				UserName = User.Claims.FirstOrDefault(c => c.Type == "https://localhost:7112/username")?.Value,
 				EmailAddress = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value,
 				ProfileImage = User.Claims.FirstOrDefault(c => c.Type == "picture")?.Value,
                 Role = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value
