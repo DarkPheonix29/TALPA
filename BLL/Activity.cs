f1aef524f24185f92f178d71f4e594ce2d88b3b4
DarkPheonix29 <kealanthal29@gmail.com>
2024-04-16 11:16:14 +0200

    activityID added

--- BLL/Activity.cs
+++ BLL/Activity.cs
@@ -13,6 +13,7 @@ namespace BLL
 {
     public class Activity(string name, string description, List<LimitationTypes> limitations, string proposingUserId, DateTime dateAdded)
     {
+        public int ActivityID { get; set; }
         public string Name { get; set; } = name;
         public string Description { get; set; } = description;
         public DateTime DateAdded { get; set; } = dateAdded;
