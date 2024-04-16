3d4658ca4429ec6405ffc914ecb2869d348585da
Bram Alders <531131@student.fontys.nl>
2024-04-16 13:32:41 +0200

    Created the pollManager class to contain all the poll methods.

--- BLL/Poll.cs
+++ BLL/Poll.cs
@@ -6,171 +6,19 @@ namespace BLL
 {
     internal class Poll(DateTime deadline)
     {
-        private int ActivityVotes1 = 0;
-        private int ActivityVotes2 = 0;
-        private int ActivityVotes3 = 0;
+        public int ActivityVotes1 = 0;
+        public int ActivityVotes2 = 0;
+        public int ActivityVotes3 = 0;
 
         //Deze moet bij de activity komen
-        private bool Won = false;
+        public bool Won = false;
 
         public int TieBreakerNumber;
         public int TripleTieBreakerNumber;
 
-        private DateTime CurrentTime = DateTime.Now;
-        private DateTime Deadline { get; set; } = deadline;
-        private List<int> activity_id { get; set; }
-
-        private void Splitactivitiess(int poll_id)
-        {
-            if (activity_id.Count == 3)
-            {
-                ActivityVotes1 = activity_id[0];
-                ActivityVotes2 = activity_id[1];
-                ActivityVotes3 = activity_id[2];
-            }
-        }
-            public void ActivityVoted(int VotedActivity)
-        {
-            switch (VotedActivity)
-            {
-                case 1:
-                    ActivityVotes1++;
-                    break;
-                case 2: 
-                    ActivityVotes2++; 
-                    break;
-                case 3:
-                    ActivityVotes3++;
-                    break;
-            }
-        }
-
-        public void DeadlineCheck()
-        {
-            if (CurrentTime >= Deadline)
-            {
-                DecideWinner();
-                GivePointsToWinner();
-            }
-        }
-
-        private void DecideWinner()
-        {
-            if (ActivityVotes1 > ActivityVotes2 && ActivityVotes1 > ActivityVotes3)
-            {
-                // verwijst naar de activity die gewonnen heeft en zet won op true
-                Won = true;
-                
-                
-            }
-
-            else if (ActivityVotes2 > ActivityVotes1 && ActivityVotes2 > ActivityVotes3)
-            {
-                // verwijst naar de activity die gewonnen heeft en zet won op true
-                Won = true;
-            }
-
-            else if (ActivityVotes3 > ActivityVotes1 && ActivityVotes3 > ActivityVotes2)
-            {
-                // verwijst naar de activity die gewonnen heeft en zet won op true
-                Won = true;
-            }
-
-            else
-            {
-                TieBreaker();
-            }
-        }
-
-        private void TieBreaker()
-        {
-            if (ActivityVotes1 == ActivityVotes2 && ActivityVotes1 == ActivityVotes3)
-            {
-                GetTripleRandom();
-                if (TieBreakerNumber == 0)
-                {
-                    // verwijst naar de activity die gewonnen heeft en zet won op true
-                    Won = true;
-                }
-
-                else if (TieBreakerNumber == 1)
-                {
-                    // verwijst naar de activity die gewonnen heeft en zet won op true
-                    Won = true;
-                }
-
-                else if (TieBreakerNumber == 2)
-                {
-                    // verwijst naar de activity die gewonnen heeft en zet won op true
-                    Won = true;
-                }
-            }
-
-            else if (ActivityVotes1 == ActivityVotes2)
-            {
-                GetDoubleRandom();
-                if (TieBreakerNumber == 0)
-                {
-                    // verwijst naar de activity die gewonnen heeft en zet won op true
-                    Won = true;
-                }
-
-                else if (TieBreakerNumber == 1)
-                {
-                    // verwijst naar de activity die gewonnen heeft en zet won op true
-                    Won = true;
-                }
-            }
-
-            else if (ActivityVotes1 == ActivityVotes3)
-            {
-                GetDoubleRandom();
-                if (TieBreakerNumber == 0)
-                {
-                    // verwijst naar de activity die gewonnen heeft en zet won op true
-                    Won = true;
-                }
-
-                else if (TieBreakerNumber == 1)
-                {
-                    // verwijst naar de activity die gewonnen heeft en zet won op true
-                    Won = true;
-                }
-            }
-            
-            else if (ActivityVotes2 == ActivityVotes3)
-            {
-                GetDoubleRandom();
-                if (TieBreakerNumber == 0)
-                {
-                    // verwijst naar de activity die gewonnen heeft en zet won op true
-                    Won = true;
-                }
-
-                else if (TieBreakerNumber == 1)
-                {
-                    // verwijst naar de activity die gewonnen heeft en zet won op true
-                    Won = true;
-                }
-            }
-        }
-
-        private void GetDoubleRandom()
-        {
-            Random rnd = new Random();
-            TieBreakerNumber = rnd.Next(1);
-        }
-
-        private void GetTripleRandom()
-        {
-            Random rnd = new Random();
-            TripleTieBreakerNumber = rnd.Next(2);
-        }
-
-        private void GivePointsToWinner()
-        {
-
-        }
+        public DateTime CurrentTime = DateTime.Now;
+        public DateTime Deadline { get; set; } = deadline;
+        public List<int> activity_id { get; set; }
     }
 }
 
---
+++ BLL/PollManager.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+	internal class PollManager
+	{
+		private void Splitactivitiess(int poll_id, Poll poll)
+		{
+			if (poll.activity_id.Count == 3)
+			{
+				poll.ActivityVotes1 = poll.activity_id[0];
+				poll.ActivityVotes2 = poll.activity_id[1];
+				poll.ActivityVotes3 = poll.activity_id[2];
+			}
+		}
+		public void ActivityVoted(int VotedActivity, Poll poll)
+		{
+			switch (VotedActivity)
+			{
+				case 1:
+					poll.ActivityVotes1++;
+					break;
+				case 2:
+					poll.ActivityVotes2++;
+					break;
+				case 3:
+					poll.ActivityVotes3++;
+					break;
+			}
+		}
+
+		public void DeadlineCheck(Poll poll)
+		{
+			if (DateTime.Now >= poll.Deadline)
+			{
+				DecideWinner(poll);
+				GivePointsToWinner();
+			}
+		}
+
+		private void DecideWinner(Poll poll)
+		{
+			if (poll.ActivityVotes1 > poll.ActivityVotes2 && poll.ActivityVotes1 > poll.ActivityVotes3)
+			{
+				// verwijst naar de activity die gewonnen heeft en zet won op true
+				poll.Won = true;
+			}
+
+			else if (poll.ActivityVotes2 > poll.ActivityVotes1 && poll.ActivityVotes2 > poll.ActivityVotes3)
+			{
+				// verwijst naar de activity die gewonnen heeft en zet won op true
+				poll.Won = true;
+			}
+
+			else if (poll.ActivityVotes3 > poll.ActivityVotes1 && poll.ActivityVotes3 > poll.ActivityVotes2)
+			{
+				// verwijst naar de activity die gewonnen heeft en zet won op true
+				poll.Won = true;
+			}
+
+			else
+			{
+				TieBreaker(poll);
+			}
+		}
+
+		private void TieBreaker(Poll poll)
+		{
+			if (poll.ActivityVotes1 == poll.ActivityVotes2 && poll.ActivityVotes1 == poll.ActivityVotes3)
+			{
+				GetTripleRandom(poll);
+				if (poll.TieBreakerNumber == 0)
+				{
+					// verwijst naar de activity die gewonnen heeft en zet won op true
+					poll.Won = true;
+				}
+
+				else if (poll.TieBreakerNumber == 1)
+				{
+					// verwijst naar de activity die gewonnen heeft en zet won op true
+					poll.Won = true;
+				}
+
+				else if (poll.TieBreakerNumber == 2)
+				{
+					// verwijst naar de activity die gewonnen heeft en zet won op true
+					poll.Won = true;
+				}
+			}
+
+			else if (poll.ActivityVotes1 == poll.ActivityVotes2)
+			{
+				GetDoubleRandom(poll);
+				if (poll.TieBreakerNumber == 0)
+				{
+					// verwijst naar de activity die gewonnen heeft en zet won op true
+					poll.Won = true;
+				}
+
+				else if (poll.TieBreakerNumber == 1)
+				{
+					// verwijst naar de activity die gewonnen heeft en zet won op true
+					poll.Won = true;
+				}
+			}
+
+			else if (poll.ActivityVotes1 == poll.ActivityVotes3)
+			{
+				GetDoubleRandom(poll);
+				if (poll.TieBreakerNumber == 0)
+				{
+					// verwijst naar de activity die gewonnen heeft en zet won op true
+					poll.Won = true;
+				}
+
+				else if (poll.TieBreakerNumber == 1)
+				{
+					// verwijst naar de activity die gewonnen heeft en zet won op true
+					poll.Won = true;
+				}
+			}
+
+			else if (poll.ActivityVotes2 == poll.ActivityVotes3)
+			{
+				GetDoubleRandom(poll);
+				if (poll.TieBreakerNumber == 0)
+				{
+					// verwijst naar de activity die gewonnen heeft en zet won op true
+					poll.Won = true;
+				}
+
+				else if (poll.TieBreakerNumber == 1)
+				{
+					// verwijst naar de activity die gewonnen heeft en zet won op true
+					poll.Won = true;
+				}
+			}
+		}
+
+		private void GetDoubleRandom(Poll poll)
+		{
+			Random rnd = new Random();
+			poll.TieBreakerNumber = rnd.Next(1);
+		}
+
+		private void GetTripleRandom(Poll poll)
+		{
+			Random rnd = new Random();
+			poll.TripleTieBreakerNumber = rnd.Next(2);
+		}
+
+		private void GivePointsToWinner()
+		{
+
+		}
+	}
+}
