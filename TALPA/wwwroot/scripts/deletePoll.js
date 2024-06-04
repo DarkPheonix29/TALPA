983d30de08048afbcf1f93d9c8b2e56d01fb70d2
Rafael Lemmen <BouncePlayR@gmail.com>
2024-06-04 10:54:30 +0200

    verwijderen voor admins

---
+++ TALPA/wwwroot/scripts/deletePoll.js
@@ -0,0 +1,6 @@
+﻿const suggestions = document.getElementById("suggestions");
+
+suggestions.addEventListener("click", function (event) {
+    var card = event.target.closest('.card')
+    alert(card.id)
+});
