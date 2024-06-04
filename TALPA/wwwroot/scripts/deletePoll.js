160d34c1d5499b39c8902828a04009356b5cfd23
Rafael Lemmen <BouncePlayR@gmail.com>
2024-06-04 11:33:27 +0200

    verwijder suggestie test

--- TALPA/wwwroot/scripts/deletePoll.js
+++ TALPA/wwwroot/scripts/deletePoll.js
@@ -4,7 +4,6 @@ const suggestionRemoveButton = document.getElementById("suggestionRemoveButton")
 
 suggestions.addEventListener("click", function (event) {
     var card = event.target.closest('.card')
-    alert(allSuggestions[card.id].name)
     suggestionLabel.textContent = allSuggestions[card.id].name
     suggestionRemoveButton.href = `/removeSuggestion/${card.id}`
     $("#removeSuggestionModal").modal("show");
