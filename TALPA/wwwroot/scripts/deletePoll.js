88c13332c81bff3434d8b86483b5e52abb5e534e
Rafael Lemmen <BouncePlayR@gmail.com>
2024-06-04 11:24:43 +0200

    verwijder test

--- TALPA/wwwroot/scripts/deletePoll.js
+++ TALPA/wwwroot/scripts/deletePoll.js
@@ -1,10 +1,13 @@
 ﻿const suggestions = document.getElementById("suggestions");
 const suggestionLabel = document.getElementById("suggestionRemoveName");
 const suggestionRemoveButton = document.getElementById("suggestionRemoveButton");
+var removeSuggestionModal = new bootstrap.Modal(document.getElementById("removeSuggestionModal"), {});
+
 
 suggestions.addEventListener("click", function (event) {
     var card = event.target.closest('.card')
     alert(allSuggestions[card.id].name)
-    suggestionLabel.textContent(allSuggestions[card.id].name)
-    suggestionRemoveButton.href = `/removeSuggestion${card.id}`
+    suggestionLabel.textContent = allSuggestions[card.id].name
+    suggestionRemoveButton.href = `/removeSuggestion/${card.id}`
+    removeSuggestionModal.show();
 });
