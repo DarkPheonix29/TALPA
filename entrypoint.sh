825ac51261e4a3d0a5ddc0e9d66753088b805ea4
M.B.J. Donners <m.donners@student.fontys.nl>
2024-06-20 12:05:38 +0200

    Update entrypoint.sh to remove credentials

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Olm4mxExne" | openconnect -bq --user=i532486 --passwd-on-stdin --no-dtls https://vdi.fhict.nl
+echo "password" | openconnect -bq --user=user --passwd-on-stdin --no-dtls https://vdi.fhict.nl
 cd /app
 dotnet TALPA.dll
